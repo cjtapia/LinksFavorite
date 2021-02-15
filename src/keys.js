256838820f5da0ad696d87d95c7102a4fc1f09cc
cjtapia <64683557+cjtapia@users.noreply.github.com>
2021-02-15 21:19:35 +0000

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'cjtapia',
+        password: 'Tapia1997',
+        database: 'db_links'
+    }
+
+};
